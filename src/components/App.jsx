5d01cc7b46bcac1d4e0debb4df21fa5810c156bf
yashrajyash <66232462+yashrajyash@users.noreply.github.com>
2021-07-15 11:52:44 +0530

    files added

---
+++ src/components/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  const [isMouseOver, setMouseState] = useState(false);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
+  function handleMouseOver() {
+    setMouseState((prevValue) => !prevValue);
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOver}
+          style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
